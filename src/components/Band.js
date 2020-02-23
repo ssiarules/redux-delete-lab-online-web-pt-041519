15c1850bc4a13456fb1f79084911d4a582340c60
Johnson <jhonsonj88@gmail.com>
2020-02-23 15:21:21 -0800

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
-  render() {
-    return(
-      <div>
-        Band Component
-      </div>
-    );
+handleOnClick = () => {
+
+  this.props.deleteBand(this.props.band.id)
+
+}
+
+render() {
+  return(
+    <div>
+      <li>{this.props.band.name}</li>
+      <button onClick={this.handleOnClick}>
+        DELETE      
+      </button>
+    </div>
+    )
   }
-};
+}
+  
 
 export default Band;
+
---
+++ src/components/Bands.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Band from './Band';
+
+
+const Bands = props => {
+    const bands = props.bands.map(band => <Band key={band.id} band={band} deleteBand={props.deleteBand} />)
+
+
+    return(
+      <div>
+        <ul>
+            {bands}
+        </ul>
+     </div>
+    );
+}
+
+
+export default Bands;
